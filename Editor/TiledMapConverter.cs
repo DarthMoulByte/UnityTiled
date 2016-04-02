63dfefa3ec5d01facfd8d8eb584a7b9f90a3edec
Nick Gravelyn <nick@gravelyn.com>
2016-04-01 22:54:54 -0700

    Initial commit with basic tileset sprite tool and map converter.

---
+++ Editor/TiledMapConverter.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEditor;
+using TiledSharp;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TiledMapConverter : EditorWindow
+{
+    private GameObject _targetObject;
+    private string _tmxFile;
+
+    void OnGUI()
+    {
+        _targetObject = (GameObject) EditorGUILayout.ObjectField("Target Object", _targetObject, typeof(GameObject), true);
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            _tmxFile = EditorGUILayout.TextField("TMX Map", _tmxFile);
+
+            if (GUILayout.Button("..."))
+            {
+                var file = EditorUtility.OpenFilePanel("Choose TMX File", Application.dataPath, "tmx");
+                if (!string.IsNullOrEmpty(file))
+                {
+                    _tmxFile = file.Replace(Application.dataPath, "");
+                    if (_tmxFile.StartsWith("/") || _tmxFile.StartsWith("\\"))
+                    {
+                        _tmxFile = _tmxFile.Substring(1);
+                    }
+                    _tmxFile = Path.Combine("Assets", _tmxFile);
+                }
+            }
+        }
+
+        if (string.IsNullOrEmpty(_tmxFile) && !File.Exists(_tmxFile))
+        {
+            EditorGUILayout.HelpBox("TMX file path required.", MessageType.Error);
+            GUI.enabled = false;
+        }
+        else
+        {
+            GUI.enabled = true;
+        }
+
+        if (GUILayout.Button("Convert"))
+        {
+            if (!_targetObject)
+            {
+                var mapName = Path.GetFileNameWithoutExtension(_tmxFile);
+                _targetObject = new GameObject(mapName);
+            }
+
+            for (int i = _targetObject.transform.childCount - 1; i >= 0; i--)
+            {
+                DestroyImmediate(_targetObject.transform.GetChild(i).gameObject);
+            }
+
+            var map = new TmxMap(_tmxFile);
+
+            var tilesetSprites = new Dictionary<int, Sprite>();
+            foreach (var tileset in map.Tilesets)
+            {
+                var sprites = AssetDatabase.LoadAllAssetsAtPath(tileset.Image.Source)
+                                           .OfType<Sprite>()
+                                           .OrderBy(s => s.name)
+                                           .ToArray();
+
+                for (int i = 0; i < sprites.Length; i++)
+                {
+                    tilesetSprites[tileset.FirstGid + i] = sprites[i];
+                }
+            }
+
+            foreach (var layer in map.Layers)
+            {
+                var layerObject = new GameObject(layer.Name);
+                layerObject.transform.SetParent(_targetObject.transform);
+
+                foreach (var tile in layer.Tiles)
+                {
+                    if (tile.Gid <= 0)
+                    {
+                        continue;
+                    }
+
+                    var tileObject = new GameObject(string.Format("Tile ({0},{1})", tile.X, tile.Y));
+                    tileObject.transform.localPosition = new Vector2(tile.X, -tile.Y);
+                    tileObject.transform.SetParent(layerObject.transform);
+
+                    var tileRenderer = tileObject.AddComponent<SpriteRenderer>();
+                    tileRenderer.sprite = tilesetSprites[tile.Gid];
+                }
+            }
+        }
+    }
+
+    [MenuItem("Window/Tiled/Map Converter")]
+    private static void OpenWindow()
+    {
+        var window = GetWindow<TiledMapConverter>();
+        window.titleContent = new GUIContent("Map Converter");
+        window.Show();
+    }
+}
---
+++ Editor/TilesetSpriteTool.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class TilesetSpriteTool : EditorWindow
+{
+    private Texture2D _tilesetTexture;
+    private int _margin;
+    private int _padding;
+    private int _tileWidth = 32;
+    private int _tileHeight = 32;
+
+    void OnGUI()
+    {
+        _tilesetTexture = (Texture2D) EditorGUILayout.ObjectField("Tileset", _tilesetTexture, typeof(Texture2D), false);
+        _tileWidth = EditorGUILayout.IntField("Tile Width", _tileWidth);
+        _tileHeight = EditorGUILayout.IntField("Tile Height", _tileHeight);
+        _margin = EditorGUILayout.IntField("Margin", _margin);
+        _padding = EditorGUILayout.IntField("Padding", _padding);
+
+        if (GUILayout.Button("Cut Sprites"))
+        {
+            var path = AssetDatabase.GetAssetPath(_tilesetTexture);
+            var importer = (TextureImporter) AssetImporter.GetAtPath(path);
+
+            importer.spriteImportMode = SpriteImportMode.Multiple;
+
+            var spritesheet = new List<SpriteMetaData>();
+
+            var gid = 0;
+            for (int y = _margin; y <= _tilesetTexture.height - _margin - _tileHeight; y += _tileHeight + _padding)
+            {
+                for (int x = _margin; x <= _tilesetTexture.width - _margin - _tileWidth; x += _tileWidth + _padding)
+                {
+                    var metadata = new SpriteMetaData();
+                    metadata.alignment = (int)SpriteAlignment.TopLeft;
+                    metadata.name = string.Format("{0}::{1:000}", _tilesetTexture.name, gid);
+                    metadata.rect = new Rect(
+                        x,
+                        _tilesetTexture.height - y - _tileHeight,
+                        _tileWidth,
+                        _tileHeight
+                    );
+                    spritesheet.Add(metadata);
+
+                    gid++;
+                }
+            }
+
+            importer.spritesheet = spritesheet.ToArray();
+
+            EditorUtility.SetDirty(importer);
+            importer.SaveAndReimport();
+        }
+    }
+
+    [MenuItem("Window/Tiled/Tileset Tool")]
+    private static void OpenWindow()
+    {
+        var window = GetWindow<TilesetSpriteTool>();
+        window.titleContent = new GUIContent("Tileset Tool");
+        window.Show();
+    }
+}
