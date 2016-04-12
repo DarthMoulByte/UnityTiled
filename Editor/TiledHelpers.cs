dc0e43828e261c477c4518479fda48df60d9c694
Nick Gravelyn <nick@gravelyn.com>
2016-04-11 19:48:00 -0700

    Spaces instead of tabs.

--- Editor/TiledHelpers.cs
+++ Editor/TiledHelpers.cs
@@ -5,21 +5,21 @@ public static class TiledHelpers
 {
     public static string GetAssetPath(string file)
     {
-		// Ensure we have a full path to the file
+        // Ensure we have a full path to the file
         file = Path.GetFullPath(file);
 
-		// Replace all backslashes with forward slashes in both the file and the data path
-		file = file.Replace("\\", "/");
-		var dataPath = Application.dataPath.Replace ("\\", "/");
+        // Replace all backslashes with forward slashes in both the file and the data path
+        file = file.Replace("\\", "/");
+        var dataPath = Application.dataPath.Replace ("\\", "/");
 
-		// Remove the data path from the start of the file path
-		file = file.Replace(dataPath, "");
+        // Remove the data path from the start of the file path
+        file = file.Replace(dataPath, "");
 
-		// If the file doesn't have a slash, add one
-		if (!file.StartsWith ("/"))
-			file = "/" + file;
-       
-		// Then put Assets at the front
-		return "Assets" + file;
+        // If the file doesn't have a slash, add one
+        if (!file.StartsWith ("/"))
+            file = "/" + file;
+
+        // Then put Assets at the front
+        return "Assets" + file;
     }
 }
